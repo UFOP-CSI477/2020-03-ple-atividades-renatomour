aadf28afd3cbdc919b16d4b4be637ce47d65dca5
renatomourart <56307607+renatomourart@users.noreply.github.com>
2020-09-22 20:41:40 -0300

    commit

--- Atividades/laravel/sistema/public/index.php
+++ Atividades/laravel/sistema/public/index.php
@@ -1,55 +1,33 @@
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+require '../vendor/autoload.php';
 
-define('LARAVEL_START', microtime(true));
+use App\Models\Estado;
+use App\Database\Connection;
+use App\Database\AdapterSQLite;
 
-/*
-|--------------------------------------------------------------------------
-| Check If Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is maintenance / demo mode via the "down" command we
-| will require this file so that any prerendered template can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
+$estado = new Estado(1, "Minas Gerais", "MG");
+var_dump($estado);
 
-if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
-    require __DIR__.'/../storage/framework/maintenance.php';
+$connection = new Connection(new AdapterSQLite());
+var_dump($connection);
+
+$connection->getAdapter()->open();
+
+// ------------------------------------------------------------
+// Implementar na view de Estados ->
+// Imprimir os estados com o adapter implementado:
+$sql = "SELECT * FROM estados";
+$result = $connection->getAdapter()->get()->query($sql);
+
+echo "<hr><ol>";
+
+while($e = $result->fetch()) {
+    echo "<li>" .$e["nome"] . "-" . $e['sigla'] . "</li>\n";
 }
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
+echo "</ol><hr>";
 
-require __DIR__.'/../vendor/autoload.php';
+// ------------------------------------------------------------
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = tap($kernel->handle(
-    $request = Request::capture()
-))->send();
-
-$kernel->terminate($request, $response);
+$connection->getAdapter()->close();
